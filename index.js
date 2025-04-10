b8d82ab191a0970c8e77ffc67fcbde6f12c7cc28
onyi-e <message.onyi@gmail.com>
2025-04-10 13:53:09 +0100

    Included more Javascript functionalities

--- index.js
+++ index.js
@@ -6,8 +6,43 @@ function userCityWeatherUpdate(response) {
   let cityElement = document.querySelector("#user-city");
   cityElement.innerHTML = response.data.city;
   tempElement.innerHTML = Math.round(temp);
-}
+  let country = document.querySelector("#country");
+  country.innerHTML = response.data.country;
+  let date = new Date(response.data.time * 1000);
+  let userDate = document.querySelector("#current-date");
+  userDate.innerHTML = convertDate(date);
 
+  let description = document.querySelector("#description");
+  description.innerHTML = response.data.condition.description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${response.data.temperature.humidity}% `;
+  let windSpeed = document.querySelector("#wind-speed");
+  let windReading = Math.round(response.data.wind.speed);
+  windSpeed.innerHTML = `${windReading}km/hr `;
+  let icon = document.querySelector("#icon");
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" />`;
+}
+function convertDate(date) {
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  if (minute < 10) {
+    minute = `0${minute}`;
+  }
+  return `${day} ${hour}:${minute},`;
+}
 function convertTemp(temp) {
   let convertedElement = document.querySelector("#temp");
   let convertTemperature = (temp * 9) / 5 + 32;
